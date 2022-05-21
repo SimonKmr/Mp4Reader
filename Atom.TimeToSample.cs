a057db8060db3638224a5ef648727ffae9c13b92
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2022-05-21 11:11:47 +0200

    added TimeToSample

---
+++ Atom.TimeToSample.cs
@@ -0,0 +1,40 @@
+﻿namespace Mp4Reader
+{
+    public partial class Atom
+    {
+        public class TimeToSample
+        {
+            public Byte Version { get; set; }
+            public UInt32 Entries { get; set; }
+            public Entry[] TimeToSampleTable { get; set; }
+
+
+            public TimeToSample(byte[] bytes)
+            {
+                Version = bytes[0];
+                Entries = BitConverter.ToUInt32(Utility.ReverseRange(bytes[4..8]));
+
+                List<Entry> entries = new List<Entry>();
+                for (int i = 0; i < Entries; i++)
+                {
+                    int start = 8 + 8 * i;
+                    int end = 8 + 8 * (i + 1);
+                    entries.Add(new Entry(bytes[start..end]));
+                }
+                TimeToSampleTable = entries.ToArray();
+            }
+
+            public class Entry
+            {
+                public UInt32 Count { get; set; }
+                public UInt32 Duration { get; set; }
+
+                public Entry(byte[] bytes)
+                {
+                    Count = BitConverter.ToUInt32(Utility.ReverseRange(bytes[0..4]));
+                    Duration = BitConverter.ToUInt32(Utility.ReverseRange(bytes[4..8]));
+                }
+            }
+        }
+    }
+}
