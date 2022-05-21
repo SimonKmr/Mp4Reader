80867aef6d5e5f26e841d7bdae909fa914b855e7
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2022-05-21 20:32:01 +0200

    added ChunkOffset and VideoMediaInfoHeader

---
+++ Atom.ChunkOffset.cs
@@ -0,0 +1,24 @@
+﻿namespace Mp4Reader
+{
+    public partial class Atom
+    {
+        public class ChunkOffset
+        {
+            public Byte Version { get; set; }
+            public UInt32 Entries { get; set; }
+            public UInt32[] ChunkOffsetTable { get; set; }
+
+            public ChunkOffset(byte[] bytes)
+            {
+                Version = bytes[0];
+                Entries = BitConverter.ToUInt32(Utility.ReverseRange(bytes[4..8]));
+
+                List<UInt32> cot = new List<UInt32>();
+                for (int i = 0; i < Entries; i++) cot.Add(
+                    BitConverter.ToUInt32(
+                        Utility.ReverseRange(bytes[(8 + 4 * i)..(8 + 4 * (i + 1))])));
+                ChunkOffsetTable = cot.ToArray();
+            }
+        }
+    }
+}
--- Mp4File.cs
+++ Mp4File.cs
@@ -48,8 +48,8 @@ namespace Mp4Reader
                 if (atoms[i].Type == "stsc") atoms[i].Data = new Atom.SampleToChunk(atoms[i].DataRaw);
                 if (atoms[i].Type == "stsd") atoms[i].Data = new Atom.SampleDescription(atoms[i].DataRaw); //not completely Implemented Missing Media Data Types
                 if (atoms[i].Type == "stsz") atoms[i].Data = new Atom.SampleSize(atoms[i].DataRaw);
-                if (atoms[i].Type == "stco") atoms[i].Data = null; //= new Atom.SampleDescription(atoms[i].DataRaw);
-                if (atoms[i].Type == "vmhd") atoms[i].Data = null; //= new Atom.SampleDescription(atoms[i].DataRaw);
+                if (atoms[i].Type == "stco") atoms[i].Data = new Atom.ChunkOffset(atoms[i].DataRaw); //= new Atom.SampleDescription(atoms[i].DataRaw);
+                if (atoms[i].Type == "vmhd") atoms[i].Data = new Atom.VideoMediaInformationHeader(atoms[i].DataRaw);
                 if (atoms[i].Type == "stss") atoms[i].Data = null; //= new Atom.SampleDescription(atoms[i].DataRaw);
                 if (atoms[i].Type == "ctts") atoms[i].Data = null; //= new Atom.SampleDescription(atoms[i].DataRaw);
             }
