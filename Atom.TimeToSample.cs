50216c33c2413c08fb20621c118c5bff894ce4a3
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2022-05-22 13:20:17 +0200

    using array instead of list

--- Atom.TimeToSample.cs
+++ Atom.TimeToSample.cs
@@ -14,14 +14,13 @@
                 Version = bytes[0];
                 Entries = BitConverter.ToUInt32(Utility.ReverseRange(bytes[4..8]));
 
-                List<Entry> entries = new List<Entry>();
+                TimeToSampleTable = new Entry[Entries];
                 for (int i = 0; i < Entries; i++)
                 {
                     int start = 8 + 8 * i;
                     int end = 8 + 8 * (i + 1);
-                    entries.Add(new Entry(bytes[start..end]));
+                    TimeToSampleTable[i] = new Entry(bytes[start..end]);
                 }
-                TimeToSampleTable = entries.ToArray();
             }
 
             public class Entry
